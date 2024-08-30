9563a48a1aa8e5869bbf25d340ba79bdca955cec
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2024-08-30 17:02:22 +0200

    added support for absence of mask

--- pipeline/data_loading.py
+++ pipeline/data_loading.py
@@ -14,6 +14,15 @@ def compute_stride(max_pixels, nb_images, npix):
     else:
         stride = max(1,int(numpy.ceil(numpy.sqrt(npix*nb_images/max_pixels))))
         return stride
+    
+def get_mask(normals_path, mask_path):
+    if mask_path is not None:
+        mask = IO.load_image(glob.glob(mask_path)[0])>0
+    else:
+        normalmap = normalmaps.rgb_to_r3(IO.load_image(glob.glob(normals_path)[0]))
+        norm = numpy.linalg.norm(normalmap,axis=-1)
+        mask = numpy.logical_and(norm>0.98, norm<1.02)
+    return mask
 
 
 def load_data(parameters_path, images_path, normals_path, mask_path):
@@ -22,7 +31,7 @@ def load_data(parameters_path, images_path, normals_path, mask_path):
         meta_parameters = yaml.safe_load(file)
     images_files_list = sorted(glob.glob(images_path))
     images_names = list(map(lambda p : os.path.basename(p),images_files_list))
-    mask = IO.load_image(glob.glob(mask_path)[0])>0
+    mask = get_mask(normals_path, mask_path)
     stride = compute_stride(meta_parameters['compute']['max_pixels'], len(images_files_list), numpy.count_nonzero(mask))
     mask = mask[::stride,::stride]
     first_image = IO.load_image(images_files_list[0],stride=stride)
