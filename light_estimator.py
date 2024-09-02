5192f022a0d8eb085e876c1edb1b5d32bfc9de45
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2024-09-02 11:45:39 +0200

    remove mask obligation

--- light_estimator.py
+++ light_estimator.py
@@ -7,7 +7,7 @@ def parse_args():
     parser.add_argument('--parameters_path', type=str, required=True, help='Path to the parameters.yaml file')
     parser.add_argument('--images_path', type=str, required=True, help='Path to the images (regex)')
     parser.add_argument('--normals_path', type=str, required=True, help='Path to the normalmap file')
-    parser.add_argument('--mask_path', type=str, required=True, help='Path to the mask file')
+    parser.add_argument('--mask_path', type=str, required=False, default=None, help='Path to the mask file')
     parser.add_argument('--out_path', type=str, required=True, help='Output path for the results')
     args = parser.parse_args()
     return args.parameters_path, args.images_path, args.normals_path, args.mask_path, args.out_path
