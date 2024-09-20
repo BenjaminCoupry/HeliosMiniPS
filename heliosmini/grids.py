8af929c47308ea30a5c14e5272a40a091e55143e
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2024-09-20 11:10:40 +0200

    fix for minimal grids

--- heliosmini/grids.py
+++ heliosmini/grids.py
@@ -5,7 +5,7 @@ def grid_over_mask(mask, npoints):
     u_min,u_max,v_min,v_max = jax.numpy.min(u_mask),jax.numpy.max(u_mask),jax.numpy.min(v_mask),jax.numpy.max(v_mask)
     nu, nv = u_max-u_min+1, v_max-v_min+1
     ratio = nu/nv 
-    nlv = int(jax.numpy.sqrt(npoints/ratio))
-    nlu = int(jax.numpy.sqrt(npoints*ratio))
+    nlv = max(2,int(jax.numpy.sqrt(npoints/ratio)))
+    nlu = max(2,int(jax.numpy.sqrt(npoints*ratio)))
     grid = jax.numpy.linspace(u_min,u_max,nlu),jax.numpy.linspace(v_min,v_max,nlv)
     return grid
